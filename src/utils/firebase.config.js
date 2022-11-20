4095fe9c4e51e813d4bfeeea5bec058e2eca4dfc
Harunur5252 <hroshid695@gmil.com>
2022-11-20 12:05:29 +0600

    project setup and initialize

---
+++ src/utils/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqQoKEUOJFQh56KBdM59hLNYBeuK0xDlU",
+  authDomain: "fir-auth-aa01d.firebaseapp.com",
+  projectId: "fir-auth-aa01d",
+  storageBucket: "fir-auth-aa01d.appspot.com",
+  messagingSenderId: "964882280519",
+  appId: "1:964882280519:web:d268bf6b75fce7af37c162"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
